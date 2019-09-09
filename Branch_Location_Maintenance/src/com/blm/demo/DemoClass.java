1ec71b31d2d13281596466ac07441d0d8212360f
Atul <Atul@ATUL-PC.in.oracle.com>
2019-09-09 17:29:57 +0530

    Demo Commit

---
+++ Branch_Location_Maintenance/src/com/blm/demo/DemoClass.java
@@ -0,0 +1,17 @@
+package com.blm.demo;
+
+/**
+ * @author Atul
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo");
+	}
+
+}
